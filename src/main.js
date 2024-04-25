4b5917b58d0fe61a853c241da0cd143cd4919425
QQQtx <1102214883@qq.com>
2024-04-25 17:13:05 +0800

    feat:登录

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './views/login.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus).mount('#app')
